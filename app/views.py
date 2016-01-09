48bf7da59151212e5eb001bddd14407de97ac6c8
Boshika Tara <boshikatara@gmail.com>
2016-01-09 07:07:46 -0800

    Created a list of dicts with authors and posts

--- app/views.py
+++ app/views.py
@@ -1,9 +1,25 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-  user = { 'nickename' : 'Boshika'}
+  user = { 'nickname' : 'Boshika'}
+  posts = [
+      {
+            'author': {'nickname': 'John'},
+            'body' : 'Living in LA!'
+      },
+      {
+            'author': {'nickname': 'Von'},
+            'body' : 'Living in SF!'
+      },
+      {
+            'author': {'nickname': 'Jim'},
+            'body' : 'Living in NOLA!'
+      }
+  ]
   return render_template('index.html',
                            title='Home',
-                           user=user)
+                           user=user,
+                           posts=posts)
