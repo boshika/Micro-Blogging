93d1b16e918ae86cec155b1aa4f7aeee06807a27
Boshika Tara <boshikatara@gmail.com>
2016-01-21 12:38:43 -0800

    Added oid.loginhandler decorator, and set-up the redirect in the login

--- app/views.py
+++ app/views.py
@@ -5,12 +5,14 @@ from .forms import LoginForm
 from .models import User
 
 @app.route('/login', methods=['GET', 'POST'])
+@oid.loginhandler
 def login():
+    if g.user is not None and g.user.is_authenticated:
+      return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for OpenID="%s", remember_me=%s' %
-              (form.openid.data, str(form.remember_me.data)))
-        return redirect('/index')
+        session['remember_me'] = form.remember_me.data
+        return oid.try_login(form.openid.data, ask_for=['nickname', 'email'])
     return render_template('login.html', 
                            title='Sign In',
                            form=form,
